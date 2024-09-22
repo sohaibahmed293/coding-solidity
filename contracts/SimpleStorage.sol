bcd72ac4cd686671d3392f7d540f5297a9bf37e9
sohaibahmed293 <sohaibahmed293@gmail.com>
2024-09-22 14:29:05 +0500

    solidity version fix and comments updated

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.24;
 
 
 contract SimpleStorage {
-    uint256 myFavouriteNumber;
 
+    // default visibilty of the function set to internal
+    uint256 myFavouriteNumber;
     
     // structure
     struct Person {
@@ -12,31 +13,27 @@ contract SimpleStorage {
         string name;
     }
 
-    // array of Persons
     // dynamic array
     Person[] public listOfPersons;
 
+    // mapping here acts as an associative array assigning fav number to a person name
     mapping(string => uint256) public nameToFavNumber;
-
-    // Person anyPerson = Person({
-    //     favouriteNumber: 7,
-    //     name: "Sohaib"
-    // });
 
     function store(uint256 _favouriteNumber) public {
         myFavouriteNumber = _favouriteNumber;
     }
 
     // view keyword means we're just reading the data from the block chain
-    // so tranaction would occur while calling this function
+    // so tranaction would not occur while calling this function unless and until called from a setter function
     // this acts as  a getter function
     function retrieve() public view returns(uint256) {
         return myFavouriteNumber;
     } 
 
     // calldata, memory and storage types of function params
-    // calldata & memory are both temporary. storage is a permanent variable
-    // memory variable can be changed to assign a new valye but not the calldata
+    // calldata & memory are both temporary means they will exist only for the duration of functioncall
+    // while storage is a permanent
+    // memory variable can be changed to assign a new value but not the calldata
 
     function addPerson(string memory _name, uint256 _favNumber) public {
         listOfPersons.push( Person(_favNumber, _name) );
